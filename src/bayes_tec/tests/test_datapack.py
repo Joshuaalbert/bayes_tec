ca6f00535f6188bd3481713aeafff11843c78dd5
joshuaalbert <albert@strw.leidenuniv.nl>
2018-07-14 19:27:10 +0200

    datapack test

---
+++ src/bayes_tec/tests/test_datapack.py
@@ -0,0 +1,10 @@
+from ..datapack import DataPack
+
+def test_datapack():
+    datapack = DataPack('test.hdf5')
+    with datapack:
+        assert datapack._contexts_open == 1
+        datapack.add_antennas()
+        datapack.add_sources([[0,0]])
+        datapack.add_freq_dep_tab('phase',times=[0,0],freqs=[0])
+        print(str(datapack.H))
