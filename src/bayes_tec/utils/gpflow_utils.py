fcfaef2558d2ace7c6d128915ffb1430d20f987b
joshuaalbert <albert@strw.leidenuniv.nl>
2018-07-19 01:35:21 +0200

    monitor the params of model

--- src/bayes_tec/utils/gpflow_utils.py
+++ src/bayes_tec/utils/gpflow_utils.py
@@ -13,14 +13,25 @@ class PrintAction(Action):
         logging.warning('{}: iteration {} likelihood {:.4f}'.format(self.text, ctx.iteration, likelihood))
 
 class SendSummary(Action):
-    def __init__(self, model, writer):
+    def __init__(self, model, writer, write_period=10):
+        self.write_period = write_period
+        self.iteration = 0
         self.model = model
         self.writer = writer
-        self.summary = tf.summary.merge_all()
+        parameters = list(self.model.parameters)
+
+        # Add scalar parameters
+        all_summaries = [tf.summary.scalar(p.pathname, tf.reshape(p.constrained_tensor, []))
+                          for p in parameters if p.size == 1]
+        all_summaries.append(tf.summary.scalar("optimisation/likelihood",
+                                               self.model._likelihood_tensor))
+        self._summary = tf.summary.merge(all_summaries)
         
     def run(self, ctx):
-        summary = ctx.session.run(self.summary)
-        self.writer.add_summary(summary,global_step=ctx.iteration)
+        if self.iteration % self.write_period == 0:
+            summary = ctx.session.run(self._summary)
+            self.writer.add_summary(summary,global_step=ctx.iteration)
+        self.iteration += 1
 
 def train_with_adam(model, learning_rate, iterations, callback=None):
     adam = AdamOptimizer(learning_rate).make_optimize_action(model)
