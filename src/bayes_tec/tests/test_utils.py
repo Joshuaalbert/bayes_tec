976dc1c2ff294be747b73178b6d0b0f32de90165
joshuaalbert <albert@strw.leidenuniv.nl>
2018-07-21 22:42:13 +0200

    weight test

--- src/bayes_tec/tests/test_utils.py
+++ src/bayes_tec/tests/test_utils.py
@@ -1,5 +1,5 @@
 from ..utils.testing_utils import make_example_datapack
-from ..utils.data_utils import define_subsets
+from ..utils.data_utils import define_subsets, calculate_weights
 import numpy as np
 import os
 
@@ -17,4 +17,13 @@ def test_make_example_datapack():
     with datapack:
         assert datapack.phase[0].shape==(10, 62, 4, 100)
     os.unlink('datapack_test_utils.hdf5')
+
+def test_weights():
+    Y = 5*np.random.normal(size=[10,1000])
+    weights = calculate_weights(Y,N=100,phase_wrap=True)
+    assert not np.any(np.isnan(weights))
+    assert np.all(weights>0)
+    weights = calculate_weights(Y,N=100,phase_wrap=False)
+    assert not np.any(np.isnan(weights))
+    assert np.all(weights>0)
 
